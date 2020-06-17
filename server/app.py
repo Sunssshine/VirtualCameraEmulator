07dc09b06d61a2aa5102aa13fa771d1cfdcf686c
Danil.Shevchenko <danil.shevchenko@mytona.com>
2020-06-17 09:44:48 +0300

    now server uses video data generator for video generation and it's even possible to download generated data

--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, send_from_directory, current_app
+import codecs
+import json
+import os
 
 app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
+
+GENERATOR_BUILD_PATH = "./bin"
 
 @app.route('/')
 def start_page():
@@ -18,3 +23,24 @@ def processing():
 @app.route('/result/')
 def result():
     return render_template('result.html')
+
+@app.route('/generate_data', methods=['GET', 'POST'])
+def generateData():
+    place = json.loads(request.data)
+    file = codecs.open(f"{GENERATOR_BUILD_PATH}/parameters.json", "w", "utf-8")
+    file.write(place)
+    file.close()
+
+    os.chdir(f"{GENERATOR_BUILD_PATH}")
+    try:
+        os.remove("result.mpg")
+    except OSError:
+        pass
+    os.system('Generator.exe')
+    os.chdir("../")
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    uploads = os.path.join(current_app.root_path, GENERATOR_BUILD_PATH)
+    return send_from_directory(directory=uploads, filename=filename, as_attachment=True, cache_timeout=0)
